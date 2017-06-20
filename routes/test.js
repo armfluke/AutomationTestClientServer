5d95af7ae751c6e0f539dabdf376ae300198a9a0
Songsirisuk <Thanakit.Songsirisuk@thomsonreuters.com>
2017-06-20 10:57:24 +0700

    Add automation test

--- routes/test.js
+++ routes/test.js
@@ -1,19 +1,35 @@
 var express = require('express');
 var router = express.Router();
 const os = require('os');
-const exec = require("child_process").exec
+const exec = require("child_process").exec;
+const fs = require('fs');
+const request = require("request");
 
 /* GET home page. */
-router.options('/', function(req, res, next) {
-    //console.log(req.params+req.body+req.query);
-    res.send(req.body);
-    /*var path = os.homedir()+"/Desktop/automationtest";
-    exec('cd '+path,function(error,stdout,stderr){
-        exec('npm start',function(error,stdout,stderr){
-            
+router.get('/', function(req, res, next) {
+    res.send({status: "OK"});
+    var path = "../automationtest";
+    console.log(path);
+    exec("cd "+path+" && npm start "+req.query.name,function(error,stdout,stderr){
+        fs.readFile('../automationtest/result.json', 'utf8', function (err,data) {
+            if (err) {
+                return console.log(err);
+            }
+            /*request.post('http://10.42.87.159:3000/submitres',
+                {json: {result: data}},
+                function(error,response,body){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        console.log(response);
+                    }
+                }
+            );*/
+            //res.send(data);
         });
-    });*/
-  //res.render('index', { title: 'Express' });
+    });
+
+    //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
