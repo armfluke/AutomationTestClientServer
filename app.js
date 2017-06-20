3f9cfa34015d397cd9f9f372374057e257fa53bf
Pansit Wattanaprasobsuk <pansit.wattanaprasobsuk@thomsonreuters.com>
2017-06-20 11:03:26 +0700

    add init

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var test = require('./routes/test');
-
+var init = require('./routes/initialize')
 init.sendIP()
 
 var app = express();
