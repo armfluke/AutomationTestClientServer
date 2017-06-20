90d16678f2181714948e602c3d6f44a886dacd82
Songsirisuk <Thanakit.Songsirisuk@thomsonreuters.com>
2017-06-20 11:56:11 +0700

    Add automation test

---
+++ automationtest/index.js
@@ -0,0 +1,10 @@
+var test = require('./test');
+var json = require('./product.json');
+
+const args = process.argv;
+
+if(args[2] != undefined && json.hasOwnProperty(args[2])){
+    test.installationTest(json[args[2]]);
+}else{
+    console.log("Please add correct product name as a first parameter (product name list is in product.json) !!!");
+}
---
+++ automationtest/test.js
@@ -0,0 +1,159 @@
+var robot = require("robotjs");
+var os = require('os');
+const fs = require('fs');
+const exec = require("child_process").exec
+
+function uninstall(){
+    var screenSize = robot.getScreenSize();
+    var height = screenSize.height;
+    var width = screenSize.width;
+    var x = width/2;
+
+    for(let y=height/2;y<height;y+=1){
+        robot.moveMouse(x,y);
+        if(robot.getPixelColor(x, y) == "ff9f40"){
+            robot.mouseClick();
+            return true;
+        }
+    }
+}
+
+function writeFile(content){
+    fs.writeFile('result.json', JSON.stringify(content), function (err) {
+        if (err){
+            console.log(err);
+        }
+    });
+}
+
+function checkProcess(json){
+    setTimeout(function(){
+        exec('tasklist | findstr /R ^'+json.installerName,function(error,stdout,stderr){
+            if(error){
+                robot.keyTap('enter');
+            }
+        });
+    }, 3000);
+}
+
+var installationTest = function(json){
+    new Promise(function(resolve,reject){   //install program
+        console.log("Start installing "+json.name);
+        var path = os.homedir()+json.installer;
+        checkProcess(json);
+        exec('start '+path,function(error,stdout,stderr){
+            if(error){
+                reject("Can't find "+json.name+" installer");
+            }
+            resolve();
+        });
+    }).then(    //check if program already install
+        function(){
+            return new Promise(function(resolve,reject){
+                setTimeout(function(){
+                    exec('tasklist | findstr /R ^'+json.processName,function(error,stdout,stderr){
+                        if(stdout != ""){
+                            resolve(json.name+" successfully install");
+                        }
+                        reject(json.name+" fail to install");
+                    });
+                }, 12000);
+            });
+        },function(error){
+            throw error;
+        }
+    ).then(     //check if program install correctly
+        function(success){
+            console.log(success);
+            return new Promise(function(resolve,reject){
+                var path = os.homedir()+json.path;
+                if (fs.existsSync(path)) {
+                    resolve(json.name+" install correctly");
+                }
+                reject(json.name+" install with wrong path");
+            });
+        },function(error){
+            throw error;
+        }
+    ).then(     //check if program create start menu
+        function(success){
+            console.log(success);
+            return new Promise(function(resolve,reject){
+                var path = os.homedir()+json.startMenu;
+                if (fs.existsSync(path)) {
+                    resolve(json.name+" already create start menu");
+                }
+                reject("Start menu don't created");
+            });
+        },function(error){
+            throw error;
+        }
+    ).then(     //kill program
+        function(success){
+            console.log(success);
+            return new Promise(function(resolve,reject){
+                exec('taskkill /IM '+json.processName+' /F',function(error,stdout,stderr){
+                    if(error){
+                        reject("Can't find process "+json.processName);
+                    }
+                    resolve();
+                });
+            });
+        },function(error){
+            throw error;
+        }
+    ).then(     //Uninstall program
+        function(){
+            return new Promise(function(resolve,reject){
+                console.log("Uninstalling "+json.name);
+                checkProcess(json);
+                var path = os.homedir()+json.installer+" --uninstall";
+                exec('start '+path,function(error,stdout,stderr){
+                    if(error){
+                        reject("Can't find "+json.name+" installer");
+                    }
+                });
+                resolve();
+            });
+        },function(error){
+            throw error;
+        }
+    ).then(     //click to uninstall
+        function(){
+            setTimeout(function(){
+                if(uninstall()){
+                    console.log(json.name+" successfully uninstall");
+                    console.log("------------------------------");
+                    console.log("!!! Automation Testing pass !!!");
+                    var content = {
+                        name : json.name,
+                        status : "Pass"
+                    }
+                    writeFile(content);
+                }else{
+                    var content = {
+                        name : json.name,
+                        status : "Fail",
+                        error : "Can't click uninstall button"
+                    }
+                    writeFile(content);
+                    console.log("------------------------------");
+                    console.log("!!! Automation Testing fail !!!");
+                }
+            }, 7000);
+        },function(error){
+            throw error;
+        }
+    ).catch(function(error){
+        var content = {
+            name : json.name,
+            status : "Fail",
+            error : error
+        }
+        writeFile(content);
+        console.log("------------------------------");
+        console.log("!!! Automation Testing fail !!!");
+    });
+}
+
+exports.installationTest = installationTest;
