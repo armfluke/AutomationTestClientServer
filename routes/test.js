c0b1b24cffaa4c64a4d24554940eb8e59dbcb88a
Pansit Wattanaprasobsuk <pansit.wattanaprasobsuk@thomsonreuters.com>
2017-06-22 16:17:01 +0700

    add test body

--- routes/test.js
+++ routes/test.js
@@ -10,48 +10,51 @@ var http = require('http')
 
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     if (status.isAvailiable()) {
         status.set('Service Unavailable')
         var path = config.testPath;
         console.log(path)
-        
+
         var name = req.param.name || req.body.name || req.query.name
-        if(!name) {
-            res.send({status: "Error", error: "Please attach product name with HTTP request"});
+        if (!name) {
+            res.send({ status: "Error", error: "Please attach product name with HTTP request" });
             status.set("Service Available");
             return;
         }
 
-        var downloadUrl = req.body.url || req.params.url || req.query.url
+        var downloadUrl = req.body.url
         if (!downloadUrl) {
-            res.send({status: "Error", error: "Please attach download url with HTTP request"})
+            res.send({ status: "Error", error: "Please attach download url with HTTP request" })
             status.set("Service Available")
             return;
         }
 
-        downloadInstaller(process.env.USERPROFILE + config.installerPath, downloadUrl, function(downloadResponse, err) {
+        var test = req.body.test || req.params.test
+
+        downloadInstaller(process.env.USERPROFILE + config.installerPath, downloadUrl, function (downloadResponse, err) {
             if (err) {
                 status.set('Service Available')
-                res.send({status: "Error", error: "Download Installer Failed (url: " + downloadUrl + ")"})
+                res.send({ status: "Error", error: "Download Installer Failed (url: " + downloadUrl + ")" })
             }
             else {
-                res.send({status: "OK"});
+                
+                res.send({ status: "OK" });
                 console.log(name)
-                exec("cd " + path + " && npm start " + name, function(error, stdout, stderr){
-                    if(error){
+                exec("cd " + path + " && npm start " + name, function (error, stdout, stderr) {
+                    if (error) {
                         console.log(error);
                     }
-                    fs.readFile('./automationtest/result.json', 'utf8', function (err,data) {
+                    fs.readFile('./automationtest/result.json', 'utf8', function (err, data) {
                         if (err) {
                             console.log(err);
                         }
-                        request.post(config.serverIP+'submitres',
-                            {json: {result: data}},
-                            function(error,response,body){
-                                if(error){
+                        request.post(config.serverIP + 'submitres',
+                            { json: { result: data } },
+                            function (error, response, body) {
+                                if (error) {
                                     console.log(error);
-                                }else{
+                                } else {
                                     console.log(response.body);
                                 }
                             }
@@ -62,8 +65,8 @@ router.get('/', function(req, res, next) {
                 });
             }
         })
-    }else {
-        res.send({status: "Server is busy, try again in few minute"})
+    } else {
+        res.send({ status: "Server is busy, try again in few minute" })
     }
 
     //res.render('index', { title: 'Express' });
@@ -78,14 +81,14 @@ function downloadInstaller(path, url, callback) {
     }
     console.log('start downloading to path ' + path + ' from ' + config.installerUrl)
     var file = fs.createWriteStream(path)
-    var request = http.get(url, function(response) {
+    var request = http.get(url, function (response) {
         response.pipe(file)
-        file.on('finish', function() {
+        file.on('finish', function () {
             file.close(callback)
             console.log('download done')
         })
-        
-    }).on('error', function(err) {
+
+    }).on('error', function (err) {
         fs.unlink(path)
         console.log(err)
         if (callback) callback(err)
